ad86e0ff9ad6b869b3ea297fa9cd19942b513950
Alex <alex@blueselene.com>
2023-01-21 16:00:09 +0100

    Add ability to copy to clipboard items

---
+++ public/clipboard.js
@@ -0,0 +1,7 @@
+function copyToClipboard(event) {
+	navigator.clipboard.writeText(event.target.innerText);
+}
+let copyElements = document.getElementsByClassName("copyToClipboard");
+for (let i = 0; i < copyElements.length; i++;) {
+	copyElements[i].addEventListener("click", copyToClipboard)
+}
