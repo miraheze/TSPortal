d7b991164cb210d86f07cf8b0bdd4c59f375fbf4
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-08-03 16:21:16 -0600

    Add migration to convert underage field to LONGTEXT

---
+++ database/migrations/2025_08_03_000001_update_dpas_underage_longtext.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateDPASUnderageLongtext extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table( 'dpas', function( Blueprint $table ) {
+			$table->longText( 'underage' )->nullable()->change();
+		} );
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table( 'dpas', function( Blueprint $table ) {
+			$table->text( 'underage' )->nullable()->change();
+		} );
+	}
+}
