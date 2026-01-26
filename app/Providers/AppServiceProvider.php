dc51fe41af2274301f20d55dcf0eabf2a2b80530
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-01-26 12:30:36 -0700

    Add

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +19,7 @@ class AppServiceProvider extends ServiceProvider
 	 */
 	public function register()
 	{
+		//
 	}
 
 	/**
@@ -23,6 +29,18 @@ class AppServiceProvider extends ServiceProvider
 	 */
 	public function boot()
 	{
+		RateLimiter::for( 'api', function ( Request $request ) {
+			return Limit::perMinute( 60 )->by( $request->user()?->id ?: $request->ip() );
+		} );
+
+		Gate::define( 'ts', function ( User $user ) {
+			return $user->hasFlag( 'ts' );
+		} );
+
+		Gate::define( 'user-manager', function ( User $user ) {
+			return $user->hasFlag( 'user-manager' );
+		} );
+
 		Paginator::useBootstrap();
 	}
 }
