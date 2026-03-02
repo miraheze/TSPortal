2defeb3ad217c8891df7a7b3b6e20e9564c0c383
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-03-02 12:58:57 -0700

    Reopened event

---
+++ app/Events/InvestigationReopened.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Investigation;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class InvestigationReopened
+{
+	use Dispatchable, InteractsWithSockets, SerializesModels;
+
+	/**
+	 * Model for event
+	 *
+	 * @var Investigation
+	 */
+	public Investigation $model;
+
+	/**
+	 * Model name
+	 *
+	 * @var string
+	 */
+	public string $name = 'Investigation';
+
+	/**
+	 * Model state
+	 *
+	 * @var string
+	 */
+	public string $state = 'reopened';
+
+	/**
+	 * Create a new event instance.
+	 *
+	 * @return void
+	 */
+	public function __construct( Investigation $investigation )
+	{
+		$this->model = $investigation;
+	}
+}
