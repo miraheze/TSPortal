e5fefbabc8f53ff14c13e6af5007b98fd135d980
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-03-14 14:24:44 -0600

    Use anonymous migrations

--- database/migrations/2022_06_01_000001_create_dpas_table.php
+++ database/migrations/2022_06_01_000001_create_dpas_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDPASTable extends Migration
-{
+return new class extends Migration {
 	/**
 	 * Run the migrations.
 	 *
@@ -33,4 +32,4 @@ class CreateDPASTable extends Migration
 	{
 		Schema::dropIfExists( 'dpas' );
 	}
-}
+};
