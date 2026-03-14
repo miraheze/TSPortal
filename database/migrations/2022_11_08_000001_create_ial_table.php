98836013bd917d85c275a29451575963edcb0c8d
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-03-14 14:26:15 -0600

    Use anonymous migrations

--- database/migrations/2022_11_08_000001_create_ial_table.php
+++ database/migrations/2022_11_08_000001_create_ial_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateIALTable extends Migration
-{
+return new class extends Migration {
 	/**
 	 * Run the migrations.
 	 *
@@ -36,4 +35,4 @@ class CreateIALTable extends Migration
 	{
 		Schema::dropIfExists( 'ial' );
 	}
-}
+};
--- database/migrations/2024_08_22_000001_update_dpas_underage.php
+++ database/migrations/2024_08_22_000001_update_dpas_underage.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateDPASUnderage extends Migration
-{
+return new class extends Migration {
 	/**
 	 * Run the migrations.
 	 *
@@ -29,4 +28,4 @@ class UpdateDPASUnderage extends Migration
 			$table->string( 'underage' )->nullable()->change();
 		} );
 	}
-}
+};
