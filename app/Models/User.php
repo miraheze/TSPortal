9df6da4c17a1ee8c220c990db968ada686a7a240
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-03-16 12:29:09 -0600

    -

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,7 @@
 namespace App\Models;
 
 use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
