567c7aabe86c91685ca676c75106ab1cb5f22108
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-03-20 13:50:30 -0600

    Use strict_types

--- app/Schedules/IALScheduler.php
+++ app/Schedules/IALScheduler.php
@@ -1,5 +1,7 @@
 <?php
 
+declare( strict_types = 1 );
+
 namespace App\Schedules;
 
 use App\Models\IAL;
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+declare( strict_types = 1 );
+
 use App\Events\DPANew;
 use App\Events\ReportNew;
 use App\Models\DPA;
