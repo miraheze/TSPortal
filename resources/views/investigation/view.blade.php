9b26b6dbf7239390284a4e0387e7712f1d11045f
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-03-20 11:35:38 -0600

    Update

--- resources/views/investigation/view.blade.php
+++ resources/views/investigation/view.blade.php
@@ -123,7 +123,7 @@
 												class="form-control" name="comments" id="comments"></textarea>
 										</div>
 										<div class="modal-footer">
-											<button class="btn btn-primary" type="submit">Submit</button>
+											<button class="btn btn-primary" type="submit">{{ __('submit') }}</button>
 										</div>
 									</div>
 								</div>
