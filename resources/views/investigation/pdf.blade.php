002a413a59d275c239aa1f54a7e35ffabc45bdfa
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-03-25 16:54:21 -0600

    Update

--- resources/views/investigation/pdf.blade.php
+++ resources/views/investigation/pdf.blade.php
@@ -161,7 +161,7 @@
         <h2>{{ __('history') }}</h2>
         @foreach($investigation->events as $event)
             <div class="box">
-                {{ $event->created }} — {{ $event->action }}
+                {{ $event->created }} — {{ __( 'events-' . $event->action ) }}
                 @if($event->comment)
                     <br>{{ $event->comment }}
                 @endif
