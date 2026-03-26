93413ae547fa8cef5935df9962118b8b515e0eb7
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-03-26 14:38:58 -0600

    Try

--- resources/views/ial.blade.php
+++ resources/views/ial.blade.php
@@ -54,16 +54,7 @@
 
 					@if ( $ials->hasPages() )
 						<nav>
-							<ul class="pagination justify-content-center">
-								<li class="page-item @disabled($ials->onFirstPage())">
-									<a class="page-link" href="@if ( $ials->onFirstPage() ) # @else {{ $ials->previousPageUrl() }} @endif" tabindex="-1">{{ __( 'nav-previous' ) }}</a>
-								</li>
-								{{ $ials->onEachSide( 5 )->links() }}
-
-								<li class="page-item @disabled(!$ials->hasMorePages())">
-									<a class="page-link" href="@if ( $ials->hasMorePages() ) {{ $ials->nextPageUrl() }} @else # @endif">{{ __( 'nav-next' ) }}</a>
-								</li>
-							</ul>
+							{{ $ials->onEachSide( 5 )->links() }}
 						</nav>
 					@endif
 				</div>
