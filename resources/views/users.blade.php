11e9e5b4e283c51d967de97a7bc58890b8b071fe
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-03-26 14:58:32 -0600

    +

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -7,6 +7,9 @@
 		<div class="card shadow">
 			<div class="card-body">
 				<div class="table-responsive table mt-2" id="dataTable" role="grid" aria-describedby="dataTable_info">
+					@if ( $users->hasPages() )
+						{{ $users->links() }}
+					@endif
 					<table class="table table-striped my-0 text-center align-middle" id="dataTable">
 						<thead>
 							<tr>
